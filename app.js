c3538c5e648f9c5b574ca5171c38d279c07e29c5
IsmaelEzequiel <kell.trollhamaren@hotmail.com>
2023-02-09 15:56:01 -0300

    pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey, bro! I\'ll be a DevOps Engineer!'));
+app.get('/', (req, res) => res.send('Hey, bro! I\'ll be a DevOps Engineer! With a Full Pipeline test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
