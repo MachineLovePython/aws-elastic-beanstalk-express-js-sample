b8125b4e39f5ddf866b7923f6e018a6b01314717
IsmaelEzequiel <kell.trollhammaren@hotmail.com>
2023-03-09 12:12:13 -0300

    this is a change

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ const port = 8080;
 app.get('/', (req, res) => res.send('Hey, bro! I\'ll be a DevOps Engineer! With a Full Pipeline test'));
 
 app.listen(port);
+
+console.log("this is a change")
+
 console.log(`App running on http://localhost:${port}`);
